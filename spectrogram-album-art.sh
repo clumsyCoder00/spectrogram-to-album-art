97740db21ea8dc9ec1999bb404c9f2a391b0fe9e
clumsyCoder00 <nuthanael.watkins@gmail.com>
2023-06-05 09:25:13 -0400

    Removed SOX dependency
    The current release of ffmpeg on git (20230313) successfully creates spectrogram with no errors, replacing a custom build of sox SOXwith a standard ffmpeg build will make the script more accessible.

--- spectrogram-album-art.sh
+++ spectrogram-album-art.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# ---- **** ----
 # DEPENDENCIES
 # ffmpeg
-# sox
 # mid3v2
-#
+
 # get more info about the file...
 # https://stackoverflow.com/questions/43415353/explanation-of-audio-stat-using-sox
 # TODO
@@ -14,46 +12,6 @@
 
 # https://linux.die.net/man/1/mid3v2
 # mid3v2 -l ( list frames in file)
-# 
-# http://jdesbonnet.blogspot.com/2014/02/sox-spectrogram-log-frequency-axis-and.html
-
-# http://webcache.googleusercontent.com/search?q=cache:DIoqGD6wbEcJ:sox.sourceforge.net/sox.html+&cd=1&hl=en&ct=clnk&gl=us
-  # y in powers of 2+1
-  # max plex resolution is 778 x 720
-  # x = 2*y with stereo
-  
-  # sox --help-effect spectrogram
-  
-  # sox options
-  # x		100-200000(800)		width of x axis in pixels
-  # y							height of y axis in pixels
-  # z		20-180(120)			Z-axis range in dB, sets range to be -num dBFS
-  # Z							Z-axis upper limit in dBFS
-  # q							Z-axis quantisation, number of different colors
-  # m							monochrome spectrogram
-  # h							Selects a high-colour palette - less visually pleasing than the 
-  #									default colour palette, but it may make it easier to differentiate
-  #									different levels. If this option is used in conjunction with −m, the
-  #									result will be a hybrid monochrome/colour palette.
-  # a							suppress the display of axis lines
-  # r							supresss the display of axis and legends
-  # t							image title text
-  # c							bottom left text
-  # o							output file, default to spectrogram.png
-  
-  # sox Statistics
-  # n stat
-  # n stats
-
-# sox variables
-# -R 80:8k -z 30 -Z -15
-
-# plex artwork ratio 780:720 1.08:1 
-# with legend and axis
-# -x 212 -y 257
-# -x 525 -y 257 (equates to 515 wide
-
-# 525/270 * 780 = 570/2 = 285
 
 OIFS="$IFS"
 IFS=$'\n'
@@ -61,9 +19,10 @@ IFS=$'\n'
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 specOut="$SCRIPTPATH/spectrogram.png"
+specSoxOut="$SCRIPTPATH/spectrogram-sox.png"
 inteOut="$SCRIPTPATH/intensity.png"
+inteFlpOut="$SCRIPTPATH/intensity-flipped.png"
 combOut="$SCRIPTPATH/combined.png"
-soxPath='/path/to/sox'
 ffmpegPath='ffmpeg'
 count=0
 procCount=0
@@ -71,44 +30,76 @@ procCount=0
 setSpectrogram() {
 	track=""
 	album=""
+  albumart=""
+	artist=""
 	processed=""
 	printf "\n$1\n"
 	dir=$(dirname "$1")
 	dirn=${dir##*/}
 	base="${1##*/}"
 	basen="${base%.*}"
-	processed=$(/usr/bin/mid3v2 -l "$1"  | grep -a "TXXX=SPECPROC=" | cut -c15-)
-	#if [ "$processed" != "1" ]
+
+	# processed=$(/usr/bin/mid3v2 -l "$1"  | grep -a "TXXX=SPECPROC=" | cut -c15-)
+	# if [ "$processed" != "1" ]
 	if [ true ]
 	then
 		procCount=$(($procCount+1))
 		printf "Processed count: $procCount\n"
-		channels=$(/usr/bin/soxi -c "$1")
-		printf "calling sox\n"
-		
-		if [ $channels == '1' ]
-		then
-			$("$soxPath" "$1" -V2 -n spectrogram -o "$specOut" -r -x 600 -y 600 -L -R 100:4k -z 25 -Z -5 -t 'MONO V3.0')
-		else
-			$("$soxPath" "$1" -V2 -n spectrogram -o "$specOut" -r -x 600 -y 300 -L -R 100:4k -z 25 -Z -5 -t 'STEREO V3.0')
-		fi
-		#sleep 1
-		printf "calling ffmpeg\n"
-		# ffmpeg intensity graph
-		$("$ffmpegPath" -y -hide_banner -loglevel error -i "$1" -filter_complex "showwavespic=s=600x600:split_channels=1:colors=#ffffff" -frames:v 1 $inteOut)
-		# ffmpeg overlay
-		$("$ffmpegPath" -y -hide_banner -loglevel error -i "$specOut" -i "$inteOut" -filter_complex "[1:v]format=argb,geq=r='r(X,Y)':a='0.5*alpha(X,Y)'[zork]; [0:v][zork]overlay" "$combOut")
-		
-		printf "calling mid3v2\n"
-		# deleting existing album art/setting new
-		$(/usr/bin/mid3v2 --delete-frames="APIC" "$1")
-		$(/usr/bin/mid3v2 -p "$combOut" "$1")
+    
+    # ---- album art section ----
+    # change to [ ! true ] to omit processing, just change metadata
+    if [ true ]
+    then
+      channels=$(/usr/bin/soxi -c "$1")
+      printf "ffmpeg - spectrogram\n"
+      
+      if [ $channels == '1' ]
+      then
+        $("$ffmpegPath" -y -hide_banner -loglevel error -i "$1" -lavfi showspectrumpic=s=600x600:mode=combined:fscale=log:start=100:stop=4000:legend=enabled:limit=-5:drange=25 "$specOut")
+      else
+        $("$ffmpegPath" -y -hide_banner -loglevel error -i "$1" -lavfi showspectrumpic=s=600x600:mode=separate:fscale=log:start=100:stop=4000:legend=disabled:limit=-5:drange=25:scale=log:gain=2 "$specOut")
+      fi
 
-		album="$dirn - $basen"
-		$(/usr/bin/mid3v2 --album=$album  "$1")
+      printf "ffmpeg - intensity\n"
+      # ffmpeg intensity graph
+      $("$ffmpegPath" -y -hide_banner -loglevel error -i "$1" -filter_complex "showwavespic=s=600x600:split_channels=1:colors=#ffffff" -frames:v 1 $inteOut)
+      printf "ffmpeg - flip intensity\n"
+      $("$ffmpegPath" -y -hide_banner -loglevel error -i $inteOut -filter_complex "vflip[flipped]" -map "[flipped]" -frames:v 1 $inteFlpOut)
+      # ffmpeg overlay
+      printf "ffmpeg - combine\n"
+      $("$ffmpegPath" -y -hide_banner -loglevel error -i "$specOut" -i "$inteFlpOut" -filter_complex "[1:v]format=argb,geq=r='r(X,Y)':a='0.5*alpha(X,Y)'[zork]; [0:v][zork]overlay" "$combOut")
+      
+      printf "calling mid3v2\n"
+      # deleting existing album art/setting new
+      $(/usr/bin/mid3v2 --delete-frames="APIC" "$1")
+      $(/usr/bin/mid3v2 -p "$combOut" "$1")
+    fi
+    # ---- album art section ----
+
+		# set album info
+		# getting existing album info
+		# track=$(/usr/bin/mid3v2 "$1" | grep ^TIT2= | cut -c6-)
+		# if [ -z "$track" ]
+		# then
+		# 	printf "Track metadata is empty, using file name\n"
+		# 	BASENAME="${1##*/}"
+		# 	track="${BASENAME%.*}"
+		# fi
+		# album=$(/usr/bin/mid3v2 "$1" | grep ^TALB= | cut -c6-)
+		# if [ "$album" != "$track" ]
+		# then
+		# 	printf "Setting track to album\n"
+		# 	$(/usr/bin/mid3v2 --album=$track "$1")
+		# fi
+    # albumart=$(/usr/bin/mid3v2 "$1" | grep ^TPE2= | cut -c6-)
+    # printf "album artist: $albumart"
+		album="$basen"
+		artist="$dirn"
 		
+    $(/usr/bin/mid3v2 --album=$album --artist=$artist --TPE2 $artist "$1")
+		    
 		# setting processed metadata
-		$(/usr/bin/mid3v2 --TXXX "SPECPROC:1" $1)
+		# $(/usr/bin/mid3v2 --TXXX "SPECPROC:1" $1)
 		printf "finished\n"
 	else
 		printf "skipping\n"
